3a0430ec9138ad80b567adb855bdf7a97a43301d
Spite <spite-vs@yandex.ru>
2023-06-19 18:39:48 +0300

    Редактирование функций удаления и изменения

--- logger.py
+++ logger.py
@@ -25,7 +25,7 @@ def input_data():
             file.write(f'{name}\n{surname}\n{phone}\n{address}\n\n')
     else:
         with open('data_second_variant.csv', 'a', encoding='utf-8') as file:
-            file.write(f'{name};{surname};{phone};{address}\n\n')
+            file.write(f'{name};{surname};{phone};{address}\n')
 
 
 def print_data():
@@ -63,12 +63,31 @@ def put_data():
         number_journal -= 1
         # Можно добавить проверку, чтобы человек не выходил за пределы записей
         print(f'Изменить данную запись\n{data_first[number_journal]}')
-        name = name_data()
-        surname = surname_data()
-        phone = phone_data()
-        address = address_data()
-        data_first = data_first[:number_journal] + [f'{name}\n{surname}\n{phone}\n{address}\n'] + \
-                     data_first[number_journal + 1:]
+
+        data_temp = data_first[number_journal].split()
+        num_prop = int(
+            input("Что меняем: \n1.Имя \n2.Фамилию \n3.Номер телефона \n4.Город \n5.Всё\n"))
+
+        if num_prop == 1:
+            data_first = data_first[:number_journal] + [f'{name_data()}\n{data_temp[1]}\n{data_temp[2]}\n{data_temp[3]}\n'] + \
+                data_first[number_journal + 1:]
+        elif num_prop == 2:
+            data_first = data_first[:number_journal] + [
+                f'{data_temp[0]}\n{surname_data()}\n{data_temp[2]}\n{data_temp[3]}\n'] + \
+                data_first[number_journal + 1:]
+        elif num_prop == 3:
+            data_first = data_first[:number_journal] + [
+                f'{data_temp[0]}\n{data_temp[1]}\n{data_temp[2]}\n{phone_data()}\n'] + \
+                data_first[number_journal + 1:]
+        elif num_prop == 4:
+            data_first = data_first[:number_journal] + [
+                f'{data_temp[0]}\n{data_temp[1]}\n{data_temp[2]}\n{address_data()}\n'] + \
+                data_first[number_journal + 1:]
+        elif num_prop == 5:
+            data_first = data_first[:number_journal] + [
+                f'{name_data()}\n{surname_data()}\n{phone_data()}\n{address_data()}\n'] + \
+                data_first[number_journal + 1:]
+
         with open('data_first_variant.csv', 'w', encoding='utf-8') as file:
             file.write(''.join(data_first))
         print('Изменения успешно сохранены!')
@@ -78,12 +97,26 @@ def put_data():
         number_journal -= 1
         # Можно добавить проверку, чтобы человек не выходил за пределы записи
         print(f'Изменить данную запись\n{data_second[number_journal]}')
-        name = name_data()
-        surname = surname_data()
-        phone = phone_data()
-        address = address_data()
-        data_second = data_second[:number_journal] + [f'{name};{surname};{phone};{address}\n'] + \
-                      data_second[number_journal + 1:]
+        data_temp = data_second[number_journal].split(';')
+        num_prop = int(
+            input("Что меняем: \n1.Имя \n2.Фамилию \n3.Номер телефона \n4.Город \n5.Всё\n"))
+
+        if num_prop == 1:
+            data_second = data_second[:number_journal] + [
+                f'{name_data()};{data_temp[1]};{data_temp[2]};{data_temp[3]}'] + data_second[number_journal + 1:]
+        elif num_prop == 2:
+            data_second = data_second[:number_journal] + [
+                f'{data_temp[0]};{surname_data()};{data_temp[2]};{data_temp[3]}'] + data_second[number_journal + 1:]
+        elif num_prop == 3:
+            data_second = data_second[:number_journal] + [
+                f'{data_temp[0]};{data_temp[1]};{phone_data()};{data_temp[3]}'] + data_second[number_journal + 1:]
+        elif num_prop == 4:
+            data_second = data_second[:number_journal] + [
+                f'{data_temp[0]};{data_temp[1]};{data_temp[2]};{address_data()}'] + data_second[number_journal + 1:]
+        elif num_prop == 5:
+            data_second = data_second[:number_journal] + [
+                f'{name_data()};{surname_data()};{phone_data()};{address_data()}'] + data_second[number_journal + 1:]
+
         with open('data_second_variant.csv', 'w', encoding='utf-8') as file:
             file.write(''.join(data_second))
         print('Изменения успешно сохранены!')  # Можно вывести конечные данные
@@ -103,7 +136,26 @@ def delete_data():
         number_journal = int(input('Введите номер записи: '))
         # Можно добавить проверку, чтобы человек не выходил за пределы записи
         print(f'Удалить данную запись\n{data_first[number_journal - 1]}')
-        data_first = data_first[:number_journal] + data_first[number_journal + 1:]
+        data_temp = data_first[number_journal].split()
+        num_prop = int(
+            input("Что удаляем: \n1.Имя \n2.Фамилию \n3.Номер телефона \n4.Город \n5.Всё\n"))
+
+        if num_prop == 1:
+            data_first = data_first[:number_journal] + [
+                f'{""}\n{data_temp[1]}\n{data_temp[2]}\n{data_temp[3]}\n'] + data_first[number_journal + 1:]
+        elif num_prop == 2:
+            data_first = data_first[:number_journal] + [
+                f'{data_temp[0]}\n{""}\n{data_temp[2]}\n{data_temp[3]}\n'] + data_first[number_journal + 1:]
+        elif num_prop == 3:
+            data_first = data_first[:number_journal] + [
+                f'{data_temp[0]}\n{data_temp[1]}\n{""}\n{data_temp[3]}\n'] + data_first[number_journal + 1:]
+        elif num_prop == 4:
+            data_first = data_first[:number_journal] + [
+                f'{data_temp[0]}\n{data_temp[1]}\n{data_temp[2]}\n{""}\n'] + data_first[number_journal + 1:]
+        elif num_prop == 5:
+            data_first = data_first[:number_journal] + \
+                data_first[number_journal + 1:]
+
         with open('data_first_variant.csv', 'w', encoding='utf-8') as file:
             file.write(''.join(data_first))
         print('Изменения успешно сохранены!')
@@ -112,7 +164,28 @@ def delete_data():
         number_journal = int(input('Введите номер записи: '))
         # Можно добавить проверку, чтобы человек не выходил за пределы записи
         print(f'Удалить данную запись\n{data_second[number_journal - 1]}')
-        data_second = data_second[:number_journal] + data_second[number_journal + 1:]
+        print(data_second)
+        data_temp = data_second[number_journal-1].split(';')
+        num_prop = int(
+            input("Что удаляем: \n1.Имя \n2.Фамилию \n3.Номер телефона \n4.Город \n5.Всё\n"))
+
+        if num_prop == 1:
+            data_second = data_second[:number_journal-1] + [
+                f'{""};{data_temp[1]};{data_temp[2]};{data_temp[3]}'] + data_second[number_journal:]
+        elif num_prop == 2:
+            data_second = data_second[:number_journal-1] + [
+                f'{data_temp[0]};{""};{data_temp[2]};{data_temp[3]}\n'] + data_second[number_journal:]
+        elif num_prop == 3:
+            data_second = data_second[:number_journal-1] + [
+                f'{data_temp[0]};{data_temp[1]};{""};{data_temp[3]}\n'] + data_second[number_journal:]
+        elif num_prop == 4:
+            data_second = data_second[:number_journal-1] + [
+                f'{data_temp[0]};{data_temp[1]};{data_temp[2]};{""}\n'] + data_second[number_journal:]
+        elif num_prop == 5:
+            data_second = data_second[:number_journal] + \
+                data_second[number_journal + 1:]
+
+        init = input('pause')
         with open('data_second_variant.csv', 'w', encoding='utf-8') as file:
             file.write(''.join(data_second))
         print('Изменения успешно сохранены!')  # Можно вывести конечные данные
